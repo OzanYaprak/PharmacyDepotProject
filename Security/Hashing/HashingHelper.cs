e7ca4ad8dd8a0b5943fdfaee3f6961be951d1a9b
Ozan Yaprak <oznyprk@gmail.com>
2026-05-31 22:50:18 +0300

    Add security helpers for JWT and password hashing
    Added SecurityKeyHelper, SigningCredentialsHelper, and HashingHelper classes for common security operations. Integrated Microsoft.IdentityModel.Tokens package to support JWT and cryptographic functionality.

---
+++ Security/Hashing/HashingHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Security.Hashing;
+
+public static class HashingHelper
+{
+    public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using HMACSHA512 hmacSHA512 = new HMACSHA512();
+
+        passwordSalt = hmacSHA512.Key;
+        passwordHash = hmacSHA512.ComputeHash(Encoding.UTF8.GetBytes(password));
+    }
+
+    public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using HMACSHA512 hmacSHA512 = new HMACSHA512(passwordSalt);
+        
+        byte[] computedHash = hmacSHA512.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        return computedHash.SequenceEqual(passwordHash);
+    }
+}
